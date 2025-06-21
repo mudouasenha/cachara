c9c97e73dda1770ffb47b02afc2f8a09e9bad4a5
Matheus Gomes <matheusgomesmtg1@gmail.com>
2025-06-21 20:10:10 -0300

    build: central package management

--- tests/Cachara.Tests/obj/Debug/net9.0/Cachara.Tests.AssemblyInfo.cs
+++ tests/Cachara.Tests/obj/Debug/net9.0/Cachara.Tests.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Cachara.Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+535deaf584a2da326c1855adf695850fcb37fdd5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+19430e78850fcbbd6341bcb40e9cf81e989e6d47")]
 [assembly: System.Reflection.AssemblyProductAttribute("Cachara.Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Cachara.Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
